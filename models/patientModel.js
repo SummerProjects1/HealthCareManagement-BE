3dcd018c62008e37fd16f007eed8889a805c8b60
Nagarjuna Kuppala <nagarjunakuppala07@gmail.com>
2018-06-10 23:59:11 -0500

    initial commit made by nagarjuna kuppala

---
+++ models/patientModel.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose');
+const patientSchema = mongoose.Schema({
+
+	userName:{
+		type: String,
+		required: true
+	},
+	firstName: {
+		type: String,
+		required: true
+	},
+	lastName: {
+		type: String,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	address: {
+		type: String,
+		required: true
+	},
+	contactNumber: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true
+	}
+});
+
+const Patient  = module.exports = mongoose.model('Patient', patientSchema);
