a66cb6315022acafa3890d180371fb77e00bbb73
nagarjunakuppala <nagarjunakuppala07@gmail.com>
2020-04-14 14:59:05 -0500

    deleted password

--- config/database.js
+++ config/database.js
@@ -2,6 +2,6 @@ module.exports = {
     database: 'mongodb://localhost:27017/healthmanagement',
     secret: 'yoursecret',
     userName: 'dev.nagarjuna2018@gmail.com',
-    password: 'Summer@2018',
-    mailOptionturnOnOff: false
+    password: '',
+    mailOptionturnOnOff: true
 }
